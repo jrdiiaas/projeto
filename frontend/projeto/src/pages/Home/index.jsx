3b0a13434cefb248a2da9409c3953df166db880b
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2024-09-11 17:09:57 -0300

    Atualização de rota da home para posts

--- frontend/projeto/src/pages/Home/index.jsx
+++ frontend/projeto/src/pages/Home/index.jsx
@@ -13,7 +13,9 @@ function Home() {
 
     const fetchPosts = async () => {
         try {
-            const response = await axios.get("http://localhost:8080/api/posts");
+            const response = await axios.get(
+                "https://projeto-production-f7e4.up.railway.app/api/posts",
+            );
             setPosts(response.data);
         } catch (error) {
             console.error("Error fetching posts:", error);
