6655f184c21c3409e5c60574229084505fd17992
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2024-09-11 15:52:35 -0300

    Atualização de arquivo env.js para deploy no Railway

--- backend/app/config/env.js
+++ backend/app/config/env.js
@@ -1,9 +1,9 @@
 const env = {
-    database: 'backend',
-    username: 'root',
-    password: 'root',
-    host: '127.0.0.1',
-    port: 3307,
+    database: process.env.DATABASE || 'backend',
+    username: process.env.USERNAME || 'root',
+    password: process.env.PASSWORD || 'root',
+    host: process.env.HOST || '127.0.0.1',
+    port: process.env.PORT || 3307,
     dialect: 'mysql',
 };
 
