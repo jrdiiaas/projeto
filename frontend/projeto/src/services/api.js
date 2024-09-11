cc5570cc2864cd6bad854c5b05e0175b1e052bf6
Alexandre Sousa Dias Junior <jrdiiaas@icloud.com>
2024-09-11 16:48:13 -0300

    Atualização arquivo api.js para deploy front no Railway

--- frontend/projeto/src/services/api.js
+++ frontend/projeto/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://projeto-production-f7e4.up.railway.app/api"
 });
 export default api;
